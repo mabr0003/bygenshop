68ffbfb173c95c0ff273ae7e9d284045c6a521fd
Marcus <mabr0003@stud.kea.dk>
2024-11-12 16:23:19 +0100

    mm

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
-  return <div></div>;
+export default function Home({ product }) {
+  return (
+    <div>
+      <article>{product.title}</article>
+    </div>
+  );
 }
