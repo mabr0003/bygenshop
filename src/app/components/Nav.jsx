9a4fef311d5d806a0711e02fa2804e8b81ddf6e7
syni0001 <143433581+syni0001@users.noreply.github.com>
2024-11-12 15:53:27 +0100

    rettler

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -25,7 +25,7 @@ export default function Navbar() {
 
   return (
     <header className="relative bg-[#e4e4e4] p-4 z-10">
-      {/* Navbar */}
+      {/* Navbar.*/}
       <nav className="flex justify-between items-center py-6">
         <div className="bg-[#bdddc1] text-white font-bold p-4 rounded-full text-lg">PureShop</div>
         <ul className="hidden md:flex space-x-6">
