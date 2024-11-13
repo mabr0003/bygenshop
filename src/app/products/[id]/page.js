ad601b46c3eb2ead3d10765929bd888dce60e096
Marcus <mabr0003@stud.kea.dk>
2024-11-13 09:53:12 +0100

    single view

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,5 +1,28 @@
-import Image from "next/image";
+"use client";
+import useSWR from "swr";
+import { useParams } from "next/navigation";
+import React, { useState, useEffect } from "react";
 
-export default function Home({ product }) {
-  return <div></div>;
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function ProductDetail() {
+  const { id } = useParams();
+
+  const [productId, setProductId] = useState(id);
+
+  useEffect(() => {
+    setProductId(id);
+  }, [id]);
+
+  const { data: product, error } = useSWR(productId ? `https://dummyjson.com/products/${productId}` : null, fetcher);
+  if (error) return <div>Der opstod en fejl...</div>;
+  if (!product) return <div>Indlæser...</div>;
+
+  return (
+    <div>
+      <article>
+        <h1>{product.title}</h1>
+      </article>
+    </div>
+  );
 }
